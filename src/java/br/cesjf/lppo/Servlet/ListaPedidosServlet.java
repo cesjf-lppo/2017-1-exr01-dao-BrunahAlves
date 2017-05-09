a4e36389b1f4be2a97112016a9ed53fd3844d33d
BrunahAlves <brunahalves94@gmail.com>
2017-05-08 22:33:29 -0300

    Adiciona ListaPedidosServlet e lista-pedidos

---
+++ src/java/br/cesjf/lppo/Servlet/ListaPedidosServlet.java
@@ -0,0 +1,45 @@
+
+package br.cesjf.lppo.Servlet;
+
+import br.cesjf.lppo.DAO.PedidoDAO;
+import br.cesjf.lppo.Pedido;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ *
+ * @author alunoces
+ */
+@WebServlet(name = "ListaContatosServlet", urlPatterns = {"/listar.html"})
+public class ListaPedidosServlet extends HttpServlet {
+
+ 
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        List<Pedido> pedidos;
+        
+         try {
+           PedidoDAO dao = new PedidoDAO();
+            pedidos = dao.listAll();
+        } catch (Exception ex) {
+            Logger.getLogger(ListaPedidosServlet.class.getName()).log(Level.SEVERE, null, ex);
+            pedidos = new ArrayList<>();
+            request.setAttribute("mensagem", ex.getLocalizedMessage());
+        }
+        
+        request.setAttribute("pedidos", pedidos);
+        request.getRequestDispatcher("WEB-INF/lista-pedidos.jsp").forward(request, response);
+    }
+
+}
